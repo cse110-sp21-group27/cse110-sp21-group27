a625bc7d5a827f2abf210e63c60760846553b3eb
Gourab <gd95148@gmail.com>
2021-06-07 17:23:30 -0700

    Focus Item Local Storage

--- mvp/script.js
+++ mvp/script.js
@@ -1,20 +1,33 @@
-import { storeData } from database.js;
+
+
+var db = window.localStorage;
 
 var focus_items = []; 
 var itemsID = 0;
 var editID = NaN;
 var cardsId = 0;
+/*
+db.getItem('numFocus')  returns total number of focus items stored
+db.getItem('focus0name');   everything else is a string
+db.getItem('focus0tags');
+db.getItem('focus0duration');
+db.getItem('focus0start');
+db.getItem('focus0end');
+db.getItem('focus0completed'); returns true or false
 
+
+
+*/
 focus_items[0] = [];
 // Default Display
-focus_items[0]["id"] = itemsID;
+/*focus_items[0]["id"] = itemsID;
 focus_items[0]["completed"] =1;
 focus_items[0]["tag"] ="Work";
 focus_items[0]["name"] ="UI Design";
 focus_items[0]["duration"] ="40";
 focus_items[0]["start"] ="2021-05-12";
 focus_items[0]["end"] ="2021-05-13";
-itemsID++;
+itemsID++;*/
 
 
 function add_new_focus() {
@@ -31,8 +44,10 @@ function close_modal(){
 
 function calculate_completed(){
     var count = 0;
-    for (i = 0 ;i < focus_items.length ;i ++) {
-        if(focus_items[i]["completed"] == 1){
+    let numFocusItems = db.getItem('numFocus');
+    for (i = 0 ;i < numFocusItems ;i ++) {
+        let phrase = "focus" + i +"completed";
+        if(db.getItem(phrase) === "true"){
             count += 1;
         }
     }
@@ -41,10 +56,12 @@ function calculate_completed(){
 
 function display(){
     document.getElementById("focus_items_list").innerHTML = "";
-    for (i = 0 ;i < focus_items.length ;i ++) {
-        var item = focus_items[i];
+    let numFocusItems = db.getItem('numFocus');
+    //console.log(db.getItem('numFocus'));
+    console.log("Display focus Items:"+numFocusItems);
+    for (i = 0 ;i < numFocusItems ;i ++) {
         var node = document.createElement("div");    
-        node.id = item.id;      
+        node.id = i;      
         node.className = "focus_single_item";    
         var top_half = document.createElement("div");   
         var bottom_half = document.createElement("div");  
@@ -66,10 +83,11 @@ function display(){
         duration.setAttribute("width", "40%");
 
         var name =  document.createElement("div");
-        name.innerHTML = item.name;
+        let phrase = "focus" + i;
+        name.innerHTML = db.getItem(phrase+"name");
         name.className = "focus_name";
         var tag =  document.createElement("div");
-        tag.innerHTML = item.tag;
+        tag.innerHTML = db.getItem(phrase+"tags");
         tag.className = "focus_tag";
 
         details.appendChild(name);
@@ -78,31 +96,31 @@ function display(){
         top_half.appendChild(img_div);  
         top_half.appendChild(details);  
 
-        if(item.duration){
+        if(db.getItem(phrase+"duration")){
             var durationtext = document.createElement("span");
-            durationtext.innerHTML = item.duration +" Minutes\n";
+            durationtext.innerHTML = db.getItem(phrase+"duration");
             duration.appendChild(durationtext); 
             var durationtext = document.createElement("br");
             duration.appendChild(durationtext); 
         }
 
-        if(item.start){
+        
             var durationtext = document.createElement("span");
-            durationtext.innerHTML = item.start;
+            durationtext.innerHTML = db.getItem(phrase+"start");
             duration.appendChild(durationtext); 
             var durationtext = document.createElement("br");
             duration.appendChild(durationtext); 
             var durationtext = document.createElement("span");
-            durationtext.innerHTML = item.end;
+            durationtext.innerHTML = db.getItem(phrase+"end");
             duration.appendChild(durationtext); 
-        }
+        
 
 
         top_half.appendChild(duration);  
 
         var done = document.createElement("img");
         done.className = "done_image";
-        if(item.completed){
+        if(db.getItem(phrase+"completed") === "true"){
             done.setAttribute('src', 'images/tick.png');
             done.setAttribute("onclick", "check("+i+",0)");
         }
@@ -133,18 +151,26 @@ function display(){
 }
 
 function check(id,set){
-    focus_items[id]['completed'] = set;
+    let isChecked;
+    if(set === 1) {
+        isChecked = "true";
+    }
+    else {
+        isChecked = "false";
+    }
+    db.setItem("focus"+ id + "completed", isChecked);
     display();
 }
 
 function edit(id){
     editID = id;
+    let phrase = "focus" + id;
     document.getElementById("focus_modal").style.display = "block";
-    document.getElementById("inp1").value = focus_items[id]['name'];
-    document.getElementById("inp2").value = focus_items[id]['tag'];
-    document.getElementById("inp3").value = focus_items[id]['duration'];
-    document.getElementById("startCalendar").value = focus_items[id]['start'];
-    document.getElementById("endCalendar").value = focus_items[id]['end'];
+    document.getElementById("inp1").value = db.getItem(phrase + "name");
+    document.getElementById("inp2").value = db.getItem(phrase + "tags");
+    document.getElementById("inp3").value = db.getItem(phrase + "duration");
+    document.getElementById("startCalendar").value = db.getItem(phrase + "start");
+    document.getElementById("endCalendar").value = db.getItem(phrase + "end");
 }
 
 
@@ -155,7 +181,8 @@ function validateMyForm(){
     var inp3 = parseInt(document.getElementById("inp3").value);
     var inp4 = document.getElementById("startCalendar").value;
     var inp5 = document.getElementById("endCalendar").value;
-    var ID= itemsID;
+    var ID= parseInt(db.getItem('numFocus'));
+    console.log("Valid Form:" + ID);
 
     if(inp4 > inp5){
         alert("Start date cannot be greater than end date.");
@@ -166,16 +193,38 @@ function validateMyForm(){
         editID = NaN;
     }
     else{
-        focus_items[ID] = [];
-        focus_items[ID]["id"] = itemsID;
+        /*focus_items[ID] = [];
+        focus_items[ID]["id"] = itemsID;*/
         itemsID++;
     }
+
+    let numFocusItems = parseInt(db.getItem("numFocus"));
+
+    if(parseInt(ID) < numFocusItems) {
+        let phrase = "focus" + ID;
+        db.setItem(phrase+ 'name', inp1);   
+        db.setItem(phrase+'tags', inp2);
+        db.setItem(phrase+'duration', inp3);
+        db.setItem(phrase+'start', inp4);
+        db.setItem(phrase+'end', inp5); 
+
+    }
+    else {
+        db.setItem("numFocus", String(numFocusItems + 1));
+        let phrase = "focus" + ID;
+        db.setItem(phrase+ 'name', inp1);   
+        db.setItem(phrase+'tags', inp2);
+        db.setItem(phrase+'duration', inp3);
+        db.setItem(phrase+'start', inp4);
+        db.setItem(phrase+'end', inp5);
+        db.setItem(phrase+"completed", "false");
+    }
     
-    focus_items[ID]["tag"] = inp2;
+    /*focus_items[ID]["tag"] = inp2;
     focus_items[ID]["name"] = inp1;
     focus_items[ID]["duration"] = inp3;
     focus_items[ID]["start"] = inp4;
-    focus_items[ID]["end"] = inp5;
+    focus_items[ID]["end"] = inp5;*/
 
     close_modal();
     display();
@@ -185,7 +234,7 @@ function validateMyForm(){
   
 }
 
-export function add_card() {
+function add_card() {
     let new_card = document.createElement("div");
     let new_date = document.createElement("p");
     let new_text = document.createElement("textarea");
@@ -208,3 +257,140 @@ function add(a, b) {
     return a + b;
 }
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function focusItemStorage() {
+    //focusItems = document.getElementsByClassName("focus_single_item");
+    let numFocusItems;
+    
+    if(db.getItem('numFocus') === null) {
+        db.setItem("numFocus","0");
+        display();   
+    } 
+    else {
+        display();
+    }
+
+    numFocusItems = db.getItem('numFocus');
+    let focusItemsArray = document.getElementsByClassName("focus_single_item");
+    
+   
+    console.log("db storage:" + db.getItem('numFocus'));
+    for (let i = 0; i < numFocusItems; i++) {
+        var key = "focus" + i;
+        storeFocusItem(focusItemsArray[i],key);
+    }
+}
+
+function storeFocusItem(focusItem,key) {
+    let wrapper = focusItem.childNodes[0];
+    let gridItems = wrapper.childNodes;
+    let checkImg = focusItem.childNodes[1].childNodes[0];
+    db.setItem(key+"name",gridItems[1].childNodes[0].innerHTML);
+    db.setItem(key+"tags", gridItems[1].childNodes[1].innerHTML);
+    db.setItem(key+"duration", gridItems[2].childNodes[0].innerHTML);
+    db.setItem(key+"start", gridItems[2].childNodes[2].innerHTML);
+    db.setItem(key+"end", gridItems[2].childNodes[4].innerHTML);
+    if(checkImg.src === "http://127.0.0.1:5502/mvp/images/tick.png") {
+        db.setItem(key+"completed","true");
+    }
+    else if (checkImg.src === "http://127.0.0.1:5502/mvp/images/tick-blank.png") {
+        db.setItem(key+"completed","false");
+    }
+    else {
+        console.log(checkImg.src);
+        db.setItem(key+"completed","error");
+    }
+   
+}
+
+
+
+ function cardStorage() {
+    let cardsArray = document.querySelectorAll("#cards_list > div");
+    let numCards = cardsArray.length;
+    db.setItem("numCards",numCards);
+    for (let i = 0; i < numCards; i++) {
+        var key = "card" + i;
+        storeCard(cardsArray[i],key);
+    }
+} 
+
+function storeCard(card,key) {
+    let cardItems = card.childNodes;
+    db.setItem(key+"date",cardItems[0].innerHTML);
+    db.setItem(key+"text",cardItems[1].innerHTML);
+}
+
+function storeData() {
+    focusItemStorage();
+    cardStorage();
+}
+
+document.getElementById("cards_button").addEventListener("change",storeData);
+
+function loadCard(card,key) {
+    let cardItems = card.childNodes;
+    cardItems[0].innerHTML = db.getItem(key+"date");
+    cardItems[1].innerHTML = db.getItem(key+"text");
+}
+
+function loadData() {
+    let numCards = db.getItem("numCards");
+    let cardsArray = document.querySelectorAll("#cards_list > div");
+    for (let i = cardsArray.length; i < numCards; i++) {
+        add_card();
+    }
+    cardsArray = document.querySelectorAll("#cards_list > div");
+    for (let i = 0; i < numCards; i++) {
+        loadCard(cardsArray[i],"card" + i);
+    }
+}
+
+document.addEventListener("DOMContentLoaded",loadData);
